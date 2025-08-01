20332d2c3db0552b07363c426f9491497f5e0c93
Swaroop-Acharya <swaroopacharya935@gmail.com>
2025-08-01 15:11:09 +0530

    New plugins hu

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+return
+  { 
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local configs = require("nvim-treesitter.configs")
+      configs.setup({
+       ensure_installed = {"lua","html","javascript","java"},
+       highlight = { enable = true},
+       indent = {enable =true},
+      })
+    end,
+  }
