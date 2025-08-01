1572c6061dc321ba20a3a7feaf417b79a17ca54f
Swaroop-Acharya <swaroopacharya935@gmail.com>
2025-08-01 17:18:06 +0530

    Neo-dap

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,12 @@
-return
-  { 
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local configs = require("nvim-treesitter.configs")
-      configs.setup({
-       ensure_installed = {"lua","html","javascript","java"},
-       highlight = { enable = true},
-       indent = {enable =true},
-      })
-    end,
-  }
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
+}
